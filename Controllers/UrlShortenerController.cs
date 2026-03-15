87df3d0c83aee36064a73c0a06ff476086f971ad
Julian Heinen <mail@julian-heinen.de>
2026-03-15 21:51:01 +0100

    feat: add controller and first dto; modify .csproj with new folder structure

---
+++ Controllers/UrlShortenerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Api.DTOs;
+
+namespace UrlShortener.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/url")]
+    public class UrlShortenerController : ControllerBase
+    {
+
+        [HttpPost("/shorten")]
+        public IActionResult ShortenUrl([FromBody] CreateUrlRequest url) {
+            return Ok("Platzhalter-URL");
+        }
+    }
+}
---
+++ DTOs/CreateUrlRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace UrlShortener.Api.DTOs
+{
+    public record CreateUrlRequest(string OriginalUrl) 
+    {
+    }
+}
