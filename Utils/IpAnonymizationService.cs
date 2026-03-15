3a1af11c4e0f95ee21dad1f4ba77b4d2f55287cd
Julian Heinen <mail@julian-heinen.de>
2026-03-15 23:27:13 +0100

    feat: add IpAnonymizationService

---
+++ Utils/IpAnonymizationService.cs
@@ -0,0 +1,36 @@
+﻿
+namespace UrlShortener.Api.Utils;
+
+public static class IpAnonymizationService
+{
+    public static string AnonymizeIp(string ipAddress)
+    {
+        if (string.IsNullOrEmpty(ipAddress))
+            return ipAddress;
+        // For IPv4, mask the last octet
+        if (ipAddress.Contains('.'))
+        {
+            var segments = ipAddress.Split('.');
+            if (segments.Length == 4)
+            {
+                segments[3] = "0"; // Mask the last octet
+                return string.Join('.', segments);
+            }
+        }
+        // For IPv6, mask the last 80 bits (the last 5 segments)
+        else if (ipAddress.Contains(':'))
+        {
+            var segments = ipAddress.Split(':');
+            if (segments.Length >= 8)
+            {
+                for (int i = 3; i < segments.Length; i++)
+                {
+                    segments[i] = "0000"; // Mask the last 5 segments
+                }
+                return string.Join(':', segments);
+            }
+        }
+        // If the format is unrecognized, return it as is
+        return ipAddress;
+    }
+}
